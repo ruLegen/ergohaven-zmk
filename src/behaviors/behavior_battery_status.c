67d441532b9e6329561ac460b982668fdc22c5ec
Stas Bes <forgame98@yandex.ru>
2026-04-13 20:48:34 +0300

    Update behavior_battery_status.c

--- src/behaviors/behavior_battery_status.c
+++ src/behaviors/behavior_battery_status.c
@@ -12,6 +12,7 @@
 
 #include <zmk/behavior.h>
 #include <zmk/battery.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -44,47 +45,98 @@ struct zmk_behavior_binding_event {
  *
  * @retval 0 If successful.
  * @retval Negative errno code if failure.
- 
+
 int zmk_behavior_invoke_binding(const struct zmk_behavior_binding *src_binding,
                                 struct zmk_behavior_binding_event event, bool pressed);
 
 */
-static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
-                                     struct zmk_behavior_binding_event event) {
 
-    uint8_t res =  zmk_battery_state_of_charge();
+                         //   0     1     2     3      4    5     6     7     8     9                            
+const uint32_t HID_KEYS[] = {0x27, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26};
+
+
+int split_reverse_digits(uint32_t num, int* digits, int max_digits) {
+    if (digits == NULL || max_digits <= 0) return 0;
+    
+    // Handle zero separately
+    if (num == 0) {
+        if (max_digits >= 1) {
+            digits[0] = 0;
+            return 1;
+        }
+        return 0;
+    }
+    
+    int index = max_digits-1;
+    int count = 0;
+    while (num > 0 && index >= 0) {
+        digits[count] = num % 10;  // Get last digit
+        num /= 10;                  // Remove last digit
+        index--;
+        count++;
+    }
+    
+    return count;
+}
+
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event)
+{
+
+    uint8_t battery_level = zmk_battery_state_of_charge();
     LOG_DBG("=====BATTER_STATUS======= PRESSED");
-    LOG_DBG("=== %d keycode 0x%02X",event.position, binding->param1);
-    LOG_DBG("=== BATTERY: %d",res);
-    LOG_DBG("=== BEHAVIOUR_DEV: %s",binding->behavior_dev);
+    LOG_DBG("=== %d keycode 0x%02X", event.position, binding->param1);
+    LOG_DBG("=== BATTERY: %d", battery_level);
+    LOG_DBG("=== BEHAVIOUR_DEV: %s", binding->behavior_dev);
     const struct device *kp = zmk_behavior_get_binding("key_press");
-    if (!kp) {
-        LOG_DBG("No behavior kp");
-    }else
+    if (kp)
     {
         LOG_DBG("===== KP FOUND %s", kp->name);
-    }
-
-    struct zmk_behavior_binding kp_binding = {
-        .behavior_dev = "key_press",
-        .param1 = 0x7001E,
-    };    
-    struct zmk_behavior_binding_event kp_binding_event = {
-        .position = event.position,
-        .timestamp = event.timestamp,
+        struct zmk_behavior_binding_event kp_binding_event = {
+            .position = event.position,
+            .timestamp = event.timestamp,
 #if IS_ENABLED(CONFIG_ZMK_SPLIT)
-        .source = event.source
+            .source = event.source
 #endif
-    };
-    //0x7001E
-    zmk_behavior_invoke_binding(&kp_binding, kp_binding_event, true);
-    zmk_behavior_invoke_binding(&kp_binding, kp_binding_event, false);
+        };
+
+        // should be more than max digits in MAX(uint8_t). 
+        // 3 should be enough for uint8_t 
+        const int max_number_of_digits = 3;
+        int digits_tmp[max_number_of_digits] = {0};     
+        int digits_count = split_reverse_digits(battery_level, &digits_tmp,max_number_of_digits);
+        if(digits_count <= 0){
+            LOG_DBG("======== DIGITS_COUNT =%d", digits_count);
+        }else{
+            
+            int start_index = max_number_of_digits - digits_count;
+            for (size_t i = start_index; i < max_number_of_digits; i++)
+            {
+                int digit = digits_tmp[i];
+                uint32_t hid_usage = ZMK_HID_USAGE(HID_USAGE_KEY, HID_KEYS[digit]);
+                LOG_DBG("======== DIGIT=%d | HID_USAGE = %d", digit, hid_usage);
+            }
+            // print digit
+            struct zmk_behavior_binding kp_binding = {
+                .behavior_dev = "key_press",
+                .param1 = hid_usage,
+            };
+            zmk_behavior_invoke_binding(&kp_binding, kp_binding_event, true);
+            zmk_behavior_invoke_binding(&kp_binding, kp_binding_event, false);
+        }
+    }
+    else
+    {
+        LOG_DBG("No behavior kp");
+    }
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
-                                      struct zmk_behavior_binding_event event) {
+                                      struct zmk_behavior_binding_event event)
+{
     LOG_DBG("=====BATTER_STATUS======= RELEASED %d keycode 0x%02X", event.position, binding->param1);
     return ZMK_BEHAVIOR_TRANSPARENT;
 }
