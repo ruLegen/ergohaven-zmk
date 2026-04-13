d568291c0258aec1fd16d7d2bd797a80794a4bd5
Stas Bes <forgame98@yandex.ru>
2026-04-13 19:26:40 +0300

    Update behavior_battery_status.c

--- src/behaviors/behavior_battery_status.c
+++ src/behaviors/behavior_battery_status.c
@@ -17,10 +17,6 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
-const struct device *zmk_behavior_get_binding(const char *name) {
-    return behavior_get_binding(name);
-}
-
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
 
